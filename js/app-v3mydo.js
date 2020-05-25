3e80099fcb9c58eafa12347448c3fb3d45262cc2
Kanzakiyuu <65440108+Kanzakiyuu@users.noreply.github.com>
2020-05-25 21:22:37 +0800

    Add files via upload

--- js/app-v3mydo.js
+++ js/app-v3mydo.js
@@ -26,12 +26,12 @@ function init() {
     if(windowWidth < 720){
         document.body.style.backgroundColor="#fff"
 		document.body.style.backgroundImage="url(https://hk1.rbqvpn.cf/2.jpg)"
-		document.body.style.backgroundSize="cover"
+		document.body.style.backgroundSize="window.screen.height window.screen.width"
     }
     if(windowWidth >= 720){
         document.body.style.backgroundColor="#fff"
 		document.body.style.backgroundImage="url(https://kanzakiyuu.github.io/Images/bj.png)"
-		document.body.style.backgroundSize="cover"
+		document.body.style.backgroundSize="window.screen.height window.screen.width"
     }
   
   document.siteName = $('title').html();
